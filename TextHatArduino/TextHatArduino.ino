14c6b7650e644753c0d659627eb004d9c027eab3
Kevin Mark <kmark937@gmail.com>
2013-10-19 18:36:38 -0400

    Improved and documented Arduino code

--- TextHatArduino/TextHatArduino.ino
+++ TextHatArduino/TextHatArduino.ino
@@ -1,34 +1,69 @@
-// include the library code:
+/************************************************************************
+ * This file is part of TextHat.                                        *
+ *                                                                      *
+ * TextHat is free software: you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * TextHat is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with TextHat.  If not, see <http://www.gnu.org/licenses/>.     *
+ ************************************************************************/
+
 #include <LiquidCrystal.h>
 #include <Wire.h>
- 
-#define REDLITE 44
-#define GREENLITE 45
-#define BLUELITE 46
- 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(30, 31, 6, 7, 8, 22);
 
+// RGB Pin Hookups
+// { redPin, greenPin, bluePin }
+unsigned int rgbPins[3] = { 44, 45, 46 };
+
+// Pin hookups for LCD
+LiquidCrystal lcd(30, 31, 6, 7, 8, 22);
+// { rows, columns }
+unsigned int lcdRowsColumns[2] = { 16, 2 };
+
+// The message delimiter
+char msgDelim = 0xA; // Line feed
+
+// If you need to change the serial info look at setup()
+
+// Number of colors available to the rainbow loop
 const int numColors = 255;
+// The rainbow loop's counter
 int counter = 0;
+
+// Used for counting the number of interrupt cycles for the text scroll
 unsigned int interruptCount = 0;
+// Counts interrupt cycles for the rainbow RGB LED loop
 unsigned int rainbowCount = 0;
+// Used for holding the scrolling text at position 0 for a certain amount of time.
 unsigned int holdAtFirstCount[2] =  {0, 0};
+// The current scrolling position of the top text
 unsigned int topPosition = 0;
+// The current scrolling position of the bottom text
 unsigned int bottomPosition = 0;
+// The current first line string
 String topString = NULL;
+// The current second line string
 String bottomString = NULL;
  
 void setup() {
+  // Assuming Bluetooth is directly connected to our hardware serial
   Serial.begin(9600);
   // set up the LCD's number of rows and columns: 
-  lcd.begin(16, 2);
+  lcd.begin(lcdRowsColumns[0], lcdRowsColumns[1]);
   // Print a message to the LCD.
   
-  pinMode(REDLITE, OUTPUT);
-  pinMode(GREENLITE, OUTPUT);
-  pinMode(BLUELITE, OUTPUT);
-  //setBacklight(50, 0, 0);
+  pinMode(rgbPins[0], OUTPUT);
+  pinMode(rgbPins[1], OUTPUT);
+  pinMode(rgbPins[2], OUTPUT);
+
+  // Default message, #YOLO on top, scrolling #SWAG on bottom
   textToLCD("#YOLO", "                #SWAG        ");
   
   /* Initialize the data output interrupt. It'll fire every millisecond
@@ -46,41 +81,44 @@ void setup() {
   TIMSK2 = 1; // Enable timer compare interrupt
   TCCR2B = 5; // Timer prescaler to 128
 }
- 
- 
+
+
+// Handles the 
 void loop() {
+  // Looping within the loop to avoid polluting the global scope with these vars
   String serialString = "";
   boolean workingOnSecondParam = false;
   String workingFirstParam = "";
-  char delim = 0xA;
   while(1) {    
     if(!Serial.available()) {
+      // No input, reset vars
       workingOnSecondParam = false;
       serialString = "";
-      //rainbowLoop();
       continue;
     }
     
+    // There's input, let's parse it
     char c = Serial.read();
-    if(!workingOnSecondParam && c == delim) {
+    if(!workingOnSecondParam && c == msgDelim) {
+      // If we're working on the top line and we've encoutered the delim...
       workingOnSecondParam = true;
-      workingFirstParam = serialString;
-      serialString = "";
-    } else if(c == delim) {
-      textToLCD(workingFirstParam, serialString);
-      workingOnSecondParam = false;
+      workingFirstParam = serialString; // The finished first line
+      serialString = ""; // Reset working string
+    } else if(c == msgDelim) {
+      // We're working on the second lien and we've encountered the delim...
+      textToLCD(workingFirstParam, serialString); // Execute textToLCD with our final inputs
+      workingOnSecondParam = false; // Reset stuff
       serialString = "";
     } else {
+      // Just another day at the office
       serialString.concat(c);
     }
     
     delay(10); // Fucking race condition?
-    
-    //textToLCD("LOL", readStringFromSerial());
-    //Serial.flush();
   }
 }
 
+// Takes two full strings and gets them ready for display
 void textToLCD(String contact, String message) {
   int length[2] = {contact.length(), message.length()};
   for(int i = 0; i < 2; i++) {
@@ -117,6 +155,7 @@ void textToLCD(String contact, String message) {
 }
 
 // Fires every millisecond
+// Handles scrolling text and changing the color of the backlight
 ISR(TIMER2_OVF_vect) {
   interruptCount++;
   rainbowCount++;
@@ -169,6 +208,8 @@ ISR(TIMER2_OVF_vect) {
   TIFR2 = 0; // Clear overflow flag
 }
 
+// Creates a String from a char array containing c
+// Faster than concatenating String objects
 String charRepeat(char c, int n) {
   char rc[n + 1];
   for(int i = 0; i < n; i++) {
@@ -177,6 +218,10 @@ String charRepeat(char c, int n) {
   return String(rc);
 }
 
+
+// Thanks to Meinaart on the Arduino forums for this!
+// Unfortunately, I'm unsure if he is the original author at this time.
+// http://forum.arduino.cc/index.php?topic=8498.0
 void rainbowLoop() {
   
   // This part takes care of displaying the
@@ -206,96 +251,79 @@ void rainbowLoop() {
   // If you uncomment this line the color changing starts from the
   // beginning when it reaches the end (animation only plays forward)
   // counter = (counter + 1) % (numColors);
-
-  //delay(80);
 }
 
+// Unsure as to the original author of this as well. Please notify me if you're aware!
 long HSBtoRGB(float _hue, float _sat, float _brightness) {
-    float red = 0.0;
-    float green = 0.0;
-    float blue = 0.0;
-    
-    if (_sat == 0.0) {
-        red = _brightness;
-        green = _brightness;
-        blue = _brightness;
-    } else {
-        if (_hue == 360.0) {
-            _hue = 0;
-        }
-
-        int slice = _hue / 60.0;
-        float hue_frac = (_hue / 60.0) - slice;
-
-        float aa = _brightness * (1.0 - _sat);
-        float bb = _brightness * (1.0 - _sat * hue_frac);
-        float cc = _brightness * (1.0 - _sat * (1.0 - hue_frac));
-        
-        switch(slice) {
-            case 0:
-                red = _brightness;
-                green = cc;
-                blue = aa;
-                break;
-            case 1:
-                red = bb;
-                green = _brightness;
-                blue = aa;
-                break;
-            case 2:
-                red = aa;
-                green = _brightness;
-                blue = cc;
-                break;
-            case 3:
-                red = aa;
-                green = bb;
-                blue = _brightness;
-                break;
-            case 4:
-                red = cc;
-                green = aa;
-                blue = _brightness;
-                break;
-            case 5:
-                red = _brightness;
-                green = aa;
-                blue = bb;
-                break;
-            default:
-                red = 0.0;
-                green = 0.0;
-                blue = 0.0;
-                break;
-        }
+  float red = 0.0;
+  float green = 0.0;
+  float blue = 0.0;
+  
+  if (_sat == 0.0) {
+    red = _brightness;
+    green = _brightness;
+    blue = _brightness;
+  } else {
+    if (_hue == 360.0) {
+      _hue = 0;
     }
-
-    long ired = red * 255.0;
-    long igreen = green * 255.0;
-    long iblue = blue * 255.0;
     
-    return long((ired << 16) | (igreen << 8) | (iblue));
+    int slice = _hue / 60.0;
+    float hue_frac = (_hue / 60.0) - slice;
+    
+    float aa = _brightness * (1.0 - _sat);
+    float bb = _brightness * (1.0 - _sat * hue_frac);
+    float cc = _brightness * (1.0 - _sat * (1.0 - hue_frac));
+    
+    switch(slice) {
+      case 0:
+        red = _brightness;
+        green = cc;
+        blue = aa;
+        break;
+      case 1:
+        red = bb;
+        green = _brightness;
+        blue = aa;
+        break;
+      case 2:
+        red = aa;
+        green = _brightness;
+        blue = cc;
+        break;
+      case 3:
+        red = aa;
+        green = bb;
+        blue = _brightness;
+        break;
+      case 4:
+        red = cc;
+        green = aa;
+        blue = _brightness;
+        break;
+      case 5:
+        red = _brightness;
+        green = aa;
+        blue = bb;
+        break;
+      default:
+        red = 0.0;
+        green = 0.0;
+        blue = 0.0;
+        break;
+    }
+  }
+  
+  long ired = red * 255.0;
+  long igreen = green * 255.0;
+  long iblue = blue * 255.0;
+  
+  return long((ired << 16) | (igreen << 8) | (iblue));
 }
- 
- 
- 
+
+// Sets the RGB backlight color from RGB values
 void setBacklight(uint8_t r, uint8_t g, uint8_t b) {
-  // normalize the red LED - its brighter than the rest!
-  //r = map(r, 0, 255, 0, 100);
-  //g = map(g, 0, 255, 0, 150);
- 
-  //r = map(r, 0, 255, 0, brightness);
-  //g = map(g, 0, 255, 0, brightness);
-  //b = map(b, 0, 255, 0, brightness);
- 
-  // common anode so invert!
-  //r = map(r, 0, 255, 255, 0);
-  //g = map(g, 0, 255, 255, 0);
-  //b = map(b, 0, 255, 255, 0);
-  //Serial.print("R = "); Serial.print(r, DEC);
-  //Serial.print(" G = "); Serial.print(g, DEC);
-  //Serial.print(" B = "); Serial.println(b, DEC);
-  analogWrite(REDLITE, r);
-  analogWrite(GREENLITE, g);
-  analogWrite(BLUELITE, b);
+  analogWrite(rgbPins[0], r);
+  analogWrite(rgbPins[1], g);
+  analogWrite(rgbPins[2], b);
 }
